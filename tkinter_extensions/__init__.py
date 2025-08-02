fbf952c8e1bf39786a17177fb8650b365e5da200
Tung-Chen Tsai <tungchentsai1753@gmail.com>
2025-08-03 01:02:00 +0800

    Restrict the Tk version to lower than 9.0.

--- tkinter_extensions/__init__.py
+++ tkinter_extensions/__init__.py
@@ -8,16 +8,27 @@ Created on Mon May 22 22:35:24 2023
 """
 
 import sys
+from tkinter import TkVersion
 from importlib.metadata import version, PackageNotFoundError
 
 from tkinter_extensions import constants, dialogs, widgets
 
 if sys.version_info < (3, 10):
     raise ImportError(
-        "tkinter_extensions requires Python 3.10 or higher. "
-        "Please upgrade your Python version."
+        'tkinter_extensions requires Python 3.10 or higher. '
+        'Please upgrade your Python version.'
     )
 
+# Tk 9.0 with Python 3.13.5 does not work well, so we restrict the version to
+# lower than 9.0.
+#TODO: Check if Tk 9 with Python 3.14 works correctly
+if TkVersion >= 9.0:
+    raise ImportError(
+        'tkinter_extensions requires Tk version lower than 9.0, but your version '
+        f'is {TkVersion}. '
+        'Please downgrade your Tk version. '
+        'You can run `python -m tkinter` to check your Tk version.'
+    )
 
 __version__: str
 try:
@@ -26,5 +37,5 @@ except PackageNotFoundError:
     __version__ = ''
 
 __all__: list[str] = ['__version__', 'constants', 'dialogs', 'widgets']
-del sys, version, PackageNotFoundError
+del sys, TkVersion, version, PackageNotFoundError
 
