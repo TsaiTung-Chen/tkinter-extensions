0680c1436c91b314d3ab64d0eb92a55bf35584ef
Tung-Chen Tsai <tungchentsai1753@gmail.com>
2025-08-02 15:00:31 +0800

    Python version check.

--- tkinter_extensions/__init__.py
+++ tkinter_extensions/__init__.py
@@ -7,16 +7,24 @@ Created on Mon May 22 22:35:24 2023
 @repository: https://github.com/TsaiTung-Chen/tkinter-extensions
 """
 
+import sys
 from importlib.metadata import version, PackageNotFoundError
 
 from tkinter_extensions import constants, dialogs, widgets
 
+if sys.version_info < (3, 10):
+    raise ImportError(
+        "tkinter_extensions requires Python 3.10 or higher. "
+        "Please upgrade your Python version."
+    )
 
+
+__version__: str
 try:
     __version__ = version('tkinter_extensions')
 except PackageNotFoundError:
     __version__ = ''
 
-__all__ = ['__version__', 'constants', 'dialogs', 'widgets']
-del version, PackageNotFoundError
+__all__: list[str] = ['__version__', 'constants', 'dialogs', 'widgets']
+del sys, version, PackageNotFoundError
 
